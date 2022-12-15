9aec95da3e22744fc969176a9a73be76484ad345
Ronillo Ang <63571980+lucifer-thebeast@users.noreply.github.com>
2022-12-15 10:05:41 +0800

    Migrate to AndroidX

--- app/src/main/java/dragosholban/com/androidpuzzlegame/PuzzleActivity.java
+++ app/src/main/java/dragosholban/com/androidpuzzlegame/PuzzleActivity.java
@@ -15,7 +15,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.ExifInterface;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
